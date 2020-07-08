986cf0425444cfc2022638d7b044accff2d997ed
Kamijoucen <lisicen12138@outlook.com>
2020-07-09 01:31:45 +0800

    完善功能

--- src/com/kamijoucen/code_deleter/CodeCommentDeleteRunner.java
+++ src/com/kamijoucen/code_deleter/CodeCommentDeleteRunner.java
@@ -3,12 +3,11 @@ package com.kamijoucen.code_deleter;
 import com.kamijoucen.code_deleter.util.LoopFile;
 
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class CodeCommentDeleteRunner {
+
+    public static final int TASK_SIZE = Runtime.getRuntime().availableProcessors() + 1;
 
     private CodeCommentDeleteRunner() {
     }
@@ -16,13 +15,23 @@ public class CodeCommentDeleteRunner {
     private static final ThreadPoolExecutor executor;
 
     static {
-        int coreNum = Runtime.getRuntime().availableProcessors() * 2;
         executor = new ThreadPoolExecutor(
-                coreNum,
-                coreNum,
+                TASK_SIZE,
+                TASK_SIZE,
                 0,
                 TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<Runnable>(128)
+                new ArrayBlockingQueue<Runnable>(128) {
+                    @Override
+                    public boolean offer(Runnable runnable) {
+                        try {
+                            put(runnable);
+                            return true;
+                        } catch (InterruptedException ex) {
+                            Thread.currentThread().interrupt();
+                            return false;
+                        }
+                    }
+                }
         );
     }
 
@@ -33,6 +42,7 @@ public class CodeCommentDeleteRunner {
         for (String file : files) {
             DeleteTask task = new DeleteTask(file, latch);
             executor.submit(task);
+//            task.run();
         }
         try {
             latch.await();
